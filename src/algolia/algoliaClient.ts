030bff1c1e0d0d9bb445e8970a45fa40a6259d19
Oleksandr Vasylenko <aleksander.vasylenko@voypost.com>
2023-10-31 01:44:37 +0200

    unpdated algolia client

--- src/algolia/algoliaClient.ts
+++ src/algolia/algoliaClient.ts
@@ -1,8 +1,8 @@
 import algoliasearch from 'algoliasearch'
 
 const AlgoliaClient = algoliasearch(
-  '28MG89MY90',
-  '3e4a44cc75e7b23457254963ea300735',
+  process.env.ALGOLIA_APP_ID || '',
+  process.env.ALGOLIA_API_KEY || '',
 )
 
 export default AlgoliaClient
